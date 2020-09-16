cc9b64befaf4a1af2bde2e649a60493823ddb335
Robin Nedergård <robin.nedergard@payfunc.com>
2020-09-16 11:06:40 +0200

    fixed incorrect amount

--- Verifier.ts
+++ Verifier.ts
@@ -153,9 +153,14 @@ export class Verifier extends model.PaymentVerifier {
 				threeDSCompInd: "Y",
 			}
 			if (paymentType != "create account") {
-				authRequest.purchaseAmount = model.Item.amount(request.items).toString()
+				const decimals = isoly.Currency.decimalDigits(request.currency) ?? 0
+				const amount = Math.round(
+					model.Item.amount(request.payment.type == "card" ? request.payment.amount ?? request.items : request.items) *
+						10 ** decimals
+				)
+				authRequest.purchaseAmount = amount.toString()
 				authRequest.purchaseCurrency = isoly.CurrencyCode.from(request.currency)
-				authRequest.purchaseExponent = (isoly.Currency.decimalDigits(request.currency) ?? 0).toString()
+				authRequest.purchaseExponent = decimals.toString()
 				authRequest.purchaseDate = ch3d2.api.model.PreciseTime.from(isoly.DateTime.now())
 			}
 			authRequest = this.appendCustomerData(request.customer, authRequest)

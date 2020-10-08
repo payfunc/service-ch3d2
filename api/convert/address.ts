6c5a5772183d83203f75b469f9ece7c1e9c24a7f
Robin Nedergård <robin.nedergard@payfunc.com>
2020-10-08 13:08:40 +0200

    updates to reflect change of model.Address.General

--- api/convert/address.ts
+++ api/convert/address.ts
@@ -9,57 +9,56 @@ export function convertAddress(input: model.Address | model.Addresses): Partial<
 	const deliveryAddress = model.Address.is(input)
 		? input
 		: input.delivery ?? input.visit ?? input.primary ?? input.billing ?? model.Address.create("SV")
-	const addrMatch =
-		model.Address.is(input) ||
-		(!model.Address.General.is(billingAddress) &&
-			!model.Address.General.is(deliveryAddress) &&
-			billingAddress.city.trim().toLocaleLowerCase() == deliveryAddress.city.trim().toLocaleLowerCase() &&
-			billingAddress.countryCode == deliveryAddress.countryCode &&
-			billingAddress.street.trim().toLocaleLowerCase() == deliveryAddress.street.trim().toLocaleLowerCase() &&
-			billingAddress.zipCode.trim().toLocaleLowerCase() == deliveryAddress.zipCode.trim().toLocaleLowerCase())
-			? "Y"
-			: "N"
-	let result: Partial<AuthRequest> = model.Address.General.is(billingAddress)
-		? {
-				addrMatch,
-				billAddrCity: "",
-				billAddrCountry: "",
-				billAddrLine1: billingAddress.address1,
-				billAddrLine2: billingAddress.address2,
-				billAddrLine3: billingAddress.address3,
-				billAddrPostCode: "",
-				billAddrState: "",
-		  }
-		: {
-				addrMatch,
-				billAddrCity: billingAddress.city,
-				billAddrCountry: isoly.CountryCode.Numeric.from(billingAddress.countryCode).toString(),
-				billAddrLine1: billingAddress.street,
-				billAddrLine2: "",
-				billAddrLine3: "",
-				billAddrPostCode: billingAddress.zipCode,
-				billAddrState: typeof (deliveryAddress as any).state == "string" ? (deliveryAddress as any).state : "",
-		  }
-	result = model.Address.General.is(deliveryAddress)
-		? {
-				...result,
-				shipAddrCity: "",
-				shipAddrCountry: "",
-				shipAddrLine1: deliveryAddress.address1,
-				shipAddrLine2: deliveryAddress.address2,
-				shipAddrLine3: deliveryAddress.address3,
-				shipAddrPostCode: "",
-				shipAddrState: "",
-		  }
-		: {
-				...result,
-				shipAddrCity: deliveryAddress.city,
-				shipAddrCountry: isoly.CountryCode.Numeric.from(deliveryAddress.countryCode).toString(),
-				shipAddrLine1: deliveryAddress.street,
-				shipAddrLine2: "",
-				shipAddrLine3: "",
-				shipAddrPostCode: deliveryAddress.zipCode,
-				shipAddrState: typeof (deliveryAddress as any).state == "string" ? (deliveryAddress as any).state : "",
-		  }
+	const addrMatch = addressesMatching(input, billingAddress, deliveryAddress)
+	let result: Partial<AuthRequest> = {
+		addrMatch,
+		billAddrCity: billingAddress.city,
+		billAddrCountry:
+			billingAddress.countryCode != "" ? isoly.CountryCode.Numeric.from(billingAddress.countryCode).toString() : "",
+		billAddrLine1: billingAddress.street ?? (model.Address.General.is(billingAddress) ? billingAddress.address1 : ""),
+		billAddrLine2: model.Address.General.is(billingAddress) && billingAddress.address2 ? billingAddress.address2 : "",
+		billAddrLine3: model.Address.General.is(billingAddress) && billingAddress.address3 ? billingAddress.address3 : "",
+		billAddrPostCode: billingAddress.zipCode,
+		billAddrState: typeof (billingAddress as any).state == "string" ? (billingAddress as any).state : "",
+	}
+	result = {
+		...result,
+		shipAddrCity: deliveryAddress.city,
+		shipAddrCountry:
+			deliveryAddress.countryCode != "" ? isoly.CountryCode.Numeric.from(deliveryAddress.countryCode).toString() : "",
+		shipAddrLine1:
+			deliveryAddress.street ?? (model.Address.General.is(deliveryAddress) ? deliveryAddress.address1 : ""),
+		shipAddrLine2:
+			model.Address.General.is(deliveryAddress) && deliveryAddress.address2 ? deliveryAddress.address2 : "",
+		shipAddrLine3:
+			model.Address.General.is(deliveryAddress) && deliveryAddress.address3 ? deliveryAddress.address3 : "",
+		shipAddrPostCode: deliveryAddress.zipCode,
+		shipAddrState: typeof (deliveryAddress as any).state == "string" ? (deliveryAddress as any).state : "",
+	}
 	return AuthRequest.limit(result)
+}
+
+export function addressesMatching(
+	input: model.Address | model.Addresses,
+	billingAddress: model.Address,
+	deliveryAddress: model.Address
+) {
+	return model.Address.is(input) ||
+		(billingAddress.city?.trim().toLocaleLowerCase() == deliveryAddress.city?.trim().toLocaleLowerCase() &&
+			billingAddress.countryCode == deliveryAddress.countryCode &&
+			billingAddress.street?.trim().toLocaleLowerCase() == deliveryAddress.street?.trim().toLocaleLowerCase() &&
+			billingAddress.zipCode?.trim().toLocaleLowerCase() == deliveryAddress.zipCode?.trim().toLocaleLowerCase() &&
+			(((billingAddress as any).state == undefined && (deliveryAddress as any).state == undefined) ||
+				(typeof (billingAddress as any).state == "string" &&
+					typeof (deliveryAddress as any).state == "string" &&
+					(billingAddress as any).state?.trim().toLocaleLowerCase() ==
+						(deliveryAddress as any).state?.trim().toLocaleLowerCase())) &&
+			((!model.Address.General.is(billingAddress) && !model.Address.General.is(deliveryAddress)) ||
+				(model.Address.General.is(billingAddress) &&
+					model.Address.General.is(deliveryAddress) &&
+					billingAddress.address1?.trim().toLocaleLowerCase() == deliveryAddress.address1?.trim().toLocaleLowerCase() &&
+					billingAddress.address2?.trim().toLocaleLowerCase() == deliveryAddress.address2?.trim().toLocaleLowerCase() &&
+					billingAddress.address3?.trim().toLocaleLowerCase() == deliveryAddress.address3?.trim().toLocaleLowerCase())))
+		? "Y"
+		: "N"
 }

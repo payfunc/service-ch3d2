07c1ed34809cd37bc5ddd67d8a66876df7c8b19f
Robin Nedergård <robin.nedergard@payfunc.com>
2021-01-26 09:57:42 +0100

    Updated to support both versions of Card.Token
    transactionId rename
    
    verify token

--- api/preauth/index.ts
+++ api/preauth/index.ts
@@ -1,5 +1,6 @@
 import * as gracely from "gracely"
 import * as authly from "authly"
+import * as card from "@payfunc/model-card"
 import { Configuration } from "../Configuration"
 import { Error } from "../Error"
 import * as connection from "../post"
@@ -7,7 +8,8 @@ import { Request } from "./Request"
 import { Response } from "./Response"
 
 async function post(configuration: Configuration, token: authly.Token): Promise<Response | Error | gracely.Error> {
-	return connection.post<Request, Response | Error | gracely.Error>(configuration, `card/${token}/ch3d2/preauth`, {})
+	const path = (await card.Card.Token.verify(token)) ? `card/ch3d2/${token}/` : `card/${token}/ch3d2/`
+	return connection.post<Request, Response | Error | gracely.Error>(configuration, path + `preauth`, {})
 }
 
 export { Request, Response, post }

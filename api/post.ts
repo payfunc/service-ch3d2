5cd69a352a0afe1602ae7c5f1f78ff16327166bc
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2021-05-06 16:28:02 +0200

    Send id to preauth parity (#63)
    * Remove legacy Verificer, Add id to preauth request
    
    * lock + package.json
    
    * Add id to all sends
    
    * fix ampersand to question mark
    
    * Fix missing path

--- api/post.ts
+++ api/post.ts
@@ -1,13 +1,15 @@
 import * as gracely from "gracely"
+import * as authly from "authly"
 import { default as fetch } from "node-fetch"
 import { Configuration } from "./Configuration"
 
 export async function post<Request, Response>(
 	configuration: Configuration,
 	endpoint: string,
-	request: Request
+	request: Request,
+	id: authly.Identifier
 ): Promise<Response> {
-	const response = await fetch(configuration.url + "/" + endpoint, {
+	const response = await fetch(configuration.url + "/" + endpoint + "?id=" + encodeURIComponent(id), {
 		method: "POST",
 		headers: { "Content-Type": "application/json; charset=utf-8", authorization: "Bearer " + configuration.key },
 		body: JSON.stringify(request),
--- index.ts
+++ index.ts
@@ -2,34 +2,38 @@ import * as gracely from "gracely"
 import * as authly from "authly"
 import * as model from "@payfunc/model"
 import * as api from "./api"
-import { Verifier } from "./Verifier"
 
 async function preauth(
 	key: authly.Token,
 	merchant: model.Key,
-	token: authly.Token
+	token: authly.Token,
+	id: authly.Identifier
 ): Promise<api.preauth.Response | api.Error | gracely.Error> {
-	return !merchant.card ? gracely.client.unauthorized() : api.preauth.post({ url: merchant.card.url, key }, token)
+	return !merchant.card ? gracely.client.unauthorized() : api.preauth.post({ url: merchant.card.url, key }, token, id)
 }
 
 async function auth(
 	key: authly.Token,
 	merchant: model.Key,
 	request: api.auth.Request,
-	token: authly.Token
+	token: authly.Token,
+	id: authly.Identifier
 ): Promise<api.auth.Response | api.Error | gracely.Error> {
-	return !merchant.card ? gracely.client.unauthorized() : api.auth.post({ url: merchant.card.url, key }, request, token)
+	return !merchant.card
+		? gracely.client.unauthorized()
+		: api.auth.post({ url: merchant.card.url, key }, request, token, id)
 }
 
 async function postauth(
 	key: authly.Token,
 	merchant: model.Key,
 	request: api.postauth.Request,
-	token: authly.Token
+	token: authly.Token,
+	id: authly.Identifier
 ): Promise<api.postauth.Response | api.Error | gracely.Error> {
 	return !merchant.card
 		? gracely.client.unauthorized()
-		: api.postauth.post({ url: merchant.card.url, key }, request, token)
+		: api.postauth.post({ url: merchant.card.url, key }, request, token, id)
 }
 
-export { api, preauth, auth, postauth, Verifier }
+export { api, preauth, auth, postauth }

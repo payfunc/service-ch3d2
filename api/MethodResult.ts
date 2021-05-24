61304d0a43da01078ea00aad5bbc51292001b922
Robin Nedergård <robin.nedergard@payfunc.com>
2021-05-24 13:35:11 +0200

    Suggested changes

--- api/MethodResult.ts
+++ api/MethodResult.ts
@@ -1,25 +1,25 @@
 import { Response as PreAuthorizationResponse } from "./preauth/Response"
 
 export interface MethodResult {
-	threeDSServerTransID: string
-	messageVersion: "2.1.0" | "2.2.0"
-	threeDSCompInd: "Y" | "U" | "N"
+	id: string
+	version: "2.1.0" | "2.2.0"
+	status: "Y" | "U" | "N"
 }
 export namespace MethodResult {
 	export function is(value: any | MethodResult): value is MethodResult {
 		return (
 			typeof value == "object" &&
-			typeof value.threeDSServerTransID == "string" &&
-			typeof value.messageVersion == "string" &&
-			["Y", "N", "U"].includes(value.threeDSCompInd)
+			typeof value.id == "string" &&
+			typeof value.version == "string" &&
+			["Y", "N", "U"].includes(value.status)
 		)
 	}
 	export function from(response: PreAuthorizationResponse, method?: "performed" | "failed"): MethodResult {
 		return {
-			threeDSServerTransID: response.threeDSServerTransID,
-			messageVersion:
+			id: response.threeDSServerTransID,
+			version:
 				response.acsStartProtocolVersion == "2.2.0" || response.dsStartProtocolVersion == "2.2.0" ? "2.2.0" : "2.1.0",
-			threeDSCompInd: !response.threeDSMethodURL ? "U" : method == "failed" ? "N" : "Y",
+			status: !response.threeDSMethodURL ? "U" : method == "failed" ? "N" : "Y",
 		}
 	}
 }
